56fe987d6e7c44e3873ec83c00bd77e23eead2fa
Rollet Coffee <maluoneill@hotmail.com>
2017-12-11 15:14:24 +0000

    first commit - full working site and prototype site

---
+++ miawebRollet/js/codjquery.js
@@ -0,0 +1,15 @@
+
+
+
+     $(document).ready(function() {
+
+            $(".show").click(function () {
+               $(this).prevAll('.mydiv').show( 1000 );
+            });
+
+            $(".hide").click(function () {
+               $(this).prevAll('.mydiv').hide( 1000 );
+            });
+                
+         });
+
